ffabfa8edb3f20b4056acdb8cc661876e52de0d3
mapo-89 <manuel@postler.de>
2025-12-14 19:28:30 +0100

    ♻️ Refactoring: enhance typing and IDE support in Facade

--- src/Facades/HomeassistantApi.php
+++ src/Facades/HomeassistantApi.php
@@ -16,7 +16,7 @@ class HomeassistantApi extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'homeassistant-api';
     }
